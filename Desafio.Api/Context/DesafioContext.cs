1c448be816d60735d3cca1796898718381f69c0b
LXSCA <lucas.edr27@gmail.com>
2024-06-20 17:09:35 -0300

    feat: updates

--- Desafio.Api/Context/DesafioContext.cs
+++ Desafio.Api/Context/DesafioContext.cs
@@ -2,8 +2,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Desafio.Api.Models;
 using Microsoft.EntityFrameworkCore;
+using Desafio.Api.Models;
 
 namespace Desafio.Api.Context
 {
@@ -11,17 +11,22 @@ namespace Desafio.Api.Context
     {
         public DesafioContext(DbContextOptions<DesafioContext> options) : base (options) {}
 
-        public DbSet<Pessoa> Pessoas { get; set;}
+        public DbSet<User> Users {get; set;}
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
 
-            modelBuilder.Entity<Pessoa>(entity =>
+            modelBuilder.Entity<User>(entity =>
             {
-               entity.HasKey(e => e.Id); 
-               // outras configuracoes abaixo;
-            
+                entity.HasKey(e => e.Id);
+                entity.Property(e => e.NomeCompleto).IsRequired();
+                entity.Property(e => e.CPF).IsRequired();
+                entity.Property(e => e.Email).IsRequired();
+                entity.Property(e => e.UserType).IsRequired();
+                entity.ToTable("Users", t => t.HasCheckConstraint("CK_UserType", "'UserType' IN ('Cliente', 'Lojista')"));
+                entity.Property(e => e.Saldo);
+
             });
         }
     }
