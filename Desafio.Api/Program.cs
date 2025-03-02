086fc402ac0a6969dda5d9e69dc97485e476d76a
LXSCA <lucas.edr27@gmail.com>
2025-03-02 14:06:16 -0300

    feat: create NotificationService

--- Desafio.Api/Program.cs
+++ Desafio.Api/Program.cs
@@ -17,6 +17,7 @@ internal class Program
         builder.Services.AddScoped<IUserService, UserService>();
         builder.Services.AddScoped<ITransferService, TransferService>();
         builder.Services.AddScoped<IUserRepository, UserRepository>();
+        builder.Services.AddSingleton<INotificationService, NotificationService>();
 
         builder.Logging.ClearProviders();
         builder.Logging.AddConsole(); 
